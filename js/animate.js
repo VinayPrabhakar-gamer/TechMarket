b46c566aa25338e6cd7d1586dd11f5fb1fb3f6aa
Vinay Prabhakar <vinaydevilbringer12@gmail.com>
2020-11-17 17:21:40 +0530

    first commit

---
+++ js/animate.js
@@ -0,0 +1,54 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar',{
+    duration: 2000,
+    origin: "bottom"
+});
+sr.reveal('.showcase-left',{
+    duration: 2000,
+    origin: "top",
+    distance: '300px'
+});
+sr.reveal('.showcase-right',{
+    duration: 2000,
+    origin: "right",
+    distance: '300px'
+});
+sr.reveal('.testimonial',{
+    duration: 2000,
+    origin: "bottom",
+});
+sr.reveal('.info1-img',{
+    duration: 2000,
+    origin: "left",
+    distance: '300px',
+    viewFactor: 0.2
+});
+sr.reveal('.info1-info',{
+    duration: 2000,
+    origin: "right",
+    distance: '300px',
+    viewFactor: 0.2
+});
+sr.reveal('.info-left',{
+    duration: 2000,
+    origin: "left",
+    distance: '300px',
+    viewFactor: 0.2
+});
+sr.reveal('.info-right',{
+    duration: 2000,
+    origin: "right",
+    distance: '300px',
+    viewFactor: 0.2
+});
+sr.reveal('#contact',{
+    duration: 2000,
+    origin: "bottom",
+    distance: '300px',
+    viewFactor: 0.2
+});
+sr.reveal('.btn',{
+    duration: 2000,
+    origin: 'bottom',
+    delay: 2000
+});
